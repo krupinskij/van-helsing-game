75236d6f85e0fef27699d8837b65c48d87329d3a
Jan Krupiński <jk.krupinski@gmail.com>
2022-08-15 18:54:31 +0200

    Add shadows on walls

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 let rot = 0;
-let posX = 0;
+let posX = 600;
 let posY = 0;
 
 const player = {
   rot: 0,
   posX: 0,
-  posY: 0,
+  posY: 600,
 
   controller: {
     ArrowLeft: {
@@ -54,8 +54,36 @@ const executeMoves = () => {
   document.documentElement.style.setProperty('--posY', `${player.posY}px`);
 };
 
+const calculateShadow = () => {
+  const walls = document.querySelectorAll('.w');
+  walls.forEach(element => {
+    let pXL = (pXR = player.posX - 600 - +element.dataset.y);
+    let pYL = (pYR = player.posY + +element.dataset.x);
+
+    switch (element.dataset.d) {
+      case 'N':
+        pYR -= +element.dataset.w;
+        break;
+      case 'S':
+        pYR += +element.dataset.w;
+        break;
+      case 'E':
+        pXR += +element.dataset.w;
+        break;
+      case 'W':
+        pXR -= +element.dataset.w;
+        break;
+    }
+    const alphaL = Math.min(1, Math.sqrt(pXL * pXL + pYL * pYL) / 1500);
+    const alphaR = Math.min(1, Math.sqrt(pXR * pXR + pYR * pYR) / 1500);
+    element.style.setProperty('--sAL', alphaR);
+    element.style.setProperty('--sAR', alphaL);
+  });
+};
+
 const animate = () => {
   executeMoves();
+  calculateShadow();
   window.requestAnimationFrame(animate);
 };
 window.requestAnimationFrame(animate);
@@ -63,7 +91,11 @@ window.requestAnimationFrame(animate);
 const room = {
   start: [2, 1],
   walls: [
-    [5, 'N'],
+    [2, 'N'],
+    [2, 'W'],
+    [1, 'N'],
+    [2, 'E'],
+    [2, 'N'],
     [3, 'E'],
     [3, 'N'],
     [3, 'E'],
@@ -76,18 +108,36 @@ const room = {
   ],
 };
 
+const room2 = {
+  start: [0, -4],
+  walls: [
+    [3, 'N'],
+    [1, 'W'],
+    [1, 'S'],
+    [1, 'W'],
+    [1, 'S'],
+    [1, 'E'],
+    [1, 'S'],
+    [1, 'E'],
+  ],
+};
+
 const createRoom = room => {
   const roomElem = document.createElement('div');
-  roomElem.className = 'room';
+  roomElem.className = 'r';
   let [x, y] = room.start;
 
   room.walls.forEach(([len, dir]) => {
-    const wall = document.createElement('div');
-    wall.dataset.dir = dir;
-    wall.style.width = `${len * 200}px`;
-    wall.style.setProperty('--x', `${x * 200}px`);
-    wall.style.setProperty('--y', `${y * 200}px`);
-    roomElem.appendChild(wall);
+    const wallElem = document.createElement('div');
+    wallElem.className = 'w';
+    wallElem.dataset.d = dir;
+    wallElem.dataset.x = x * 200;
+    wallElem.dataset.y = y * 200;
+    wallElem.dataset.w = len * 200;
+    wallElem.style.width = `${len * 200}px`;
+    wallElem.style.setProperty('--x', `${x * 200}px`);
+    wallElem.style.setProperty('--y', `${y * 200}px`);
+    roomElem.appendChild(wallElem);
 
     switch (dir) {
       case 'N':
@@ -108,3 +158,4 @@ const createRoom = room => {
 };
 
 createRoom(room);
+createRoom(room2);
